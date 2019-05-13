093f008e336b95b7c9373a4d901c3fff4c2ba7f2
Zane Stearman <zstearman@mail.greenriver.edu>
2019-05-13 19:56:12 -0400

    Fin

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
 
-//Start a session
-session_start();
 
 //Turn on error reporting
 ini_set('display_errors', 1);
@@ -11,12 +9,16 @@ error_reporting(E_ALL);
 require_once('vendor/autoload.php');
 require_once('model/validate.php');
 
+//Start a session
+session_start();
+
 //Instantiate Fat-Free
 $f3 = Base::instance();
 
 //Turn on Fat-Free error reporting
 $f3->set('DEBUG', 3);
 
+$f3->set('options', ['This midterm is awesome', 'I love midterms', 'Looks like somebody has a case of the Mondays']);
 
 
 //Define a default route
@@ -30,15 +32,15 @@ $f3->route('GET /', function() {
 $f3->route('GET|POST /survey', function($f3) {
 
 //Define arrays
-    $f3->set('options', ['This midterm is awesome', 'I love midterms', 'Looks like somebody has a case of the Mondays']);
 
-
+    if (!empty($_POST)){
         //Get data from form
         $name = $_POST['name'];
         $options = $_POST['options'];
 
         //Add data to hive
         $f3->set('options', $options);
+        $f3->set('name', $name);
 
         //If data is valid
         if (validForm()) {
@@ -47,23 +49,20 @@ $f3->route('GET|POST /survey', function($f3) {
             $_SESSION['options'] = $options;
             $_SESSION['name'] = $name;
 
-            if (empty($options)) {
-                $_SESSION['options'] = "Please fill out the survey";
-            } else {
-                $_SESSION['options'] = implode(', ', $options);
-            }
-
             //Redirect to Summary
             $f3->reroute('/summary');
         }
-//    }
+    }
         //Display order form
         $view = new Template();
         echo $view->render('views/survey.html');
 });
 
 //Define a summary route
-$f3->route('GET /summary', function() {
+$f3->route('GET /summary', function($f3) {
+
+    $f3->set('testArray', implode(', ', $_SESSION['options']));
+
 
     //Display summary
     $view = new Template();
