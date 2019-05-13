348c81fe433989c63ace3fcbec3764a8cd7ae084
Zane Stearman <zstearman@mail.greenriver.edu>
2019-05-13 17:55:45 -0400

    adjust index

---
+++ index.php
@@ -0,0 +1,74 @@
+<?php
+
+//Start a session
+session_start();
+
+//Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//Required file
+require_once('vendor/autoload.php');
+require_once('model/validate.php');
+
+//Instantiate Fat-Free
+$f3 = Base::instance();
+
+//Turn on Fat-Free error reporting
+$f3->set('DEBUG', 3);
+
+
+
+//Define a default route
+$f3->route('GET /', function() {
+
+    echo "<header>Midterm Survey</header>";
+    echo "<a href='survey'>Take my Midterm Survey</a>";
+});
+
+//Define an order route
+$f3->route('GET|POST /survey', function($f3) {
+
+//Define arrays
+    $f3->set('options', ['This midterm is awesome', 'I love midterms', 'Looks like somebody has a case of the Mondays']);
+
+
+        //Get data from form
+        $name = $_POST['name'];
+        $options = $_POST['options'];
+
+        //Add data to hive
+        $f3->set('options', $options);
+
+        //If data is valid
+        if (validForm()) {
+
+            //Write data to Session
+            $_SESSION['options'] = $options;
+            $_SESSION['name'] = $name;
+
+            if (empty($options)) {
+                $_SESSION['options'] = "Please fill out the survey";
+            } else {
+                $_SESSION['options'] = implode(', ', $options);
+            }
+
+            //Redirect to Summary
+            $f3->reroute('/summary');
+        }
+//    }
+        //Display order form
+        $view = new Template();
+        echo $view->render('views/survey.html');
+});
+
+//Define a summary route
+$f3->route('GET /summary', function() {
+
+    //Display summary
+    $view = new Template();
+    echo $view->render('views/summary.html');
+});
+
+//Run Fat-Free
+$f3->run();
